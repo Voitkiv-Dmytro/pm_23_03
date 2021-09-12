2c02d298d443438faa2d0ae5d243cf9b5923c66b
Voitkiv-Dmytro <dmytro.voitkiv.pm.2020@lpnu.ua>
2021-09-13 00:46:28 +0300

    Voitkiv-Dmytro

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+//Підключаємо gulp
+var gulp = require ("gulp");
+//Створюємо тестовий таск
+gulp.task ('testTask', function () {
+ console.log ('This is a test task!');
+});
+//Запуск тасків за замовчуванням
+gulp.task ("default", ["testTask"]);
+var gulp = require ("gulp");
+//додаткові плагіни Gulp
+var sass = require ("gulp-sass"), //конвертує SASS в CSS
+ cssnano = require ("gulp-cssnano"), //мінімізація CSS
+ autoprefixer = require ('gulp-autoprefixer'), //додавання префіксів в
+ //CSS для підтримки
+ //старих браузерів
+ imagemin = require ('gulp-imagemin'), //стиснення зображень
+concat = require ("gulp-concat"), //об'єднання файлів - конкатенація
+ uglify = require ("gulp-uglify"), //мінімізація javascript
+ rename = require ("gulp-rename"); //перейменування файлів
