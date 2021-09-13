aaa5625a1c92f5fc2a9f956e2b100b7a21e9b79b
Voitkiv-Dmytro <dmytro.voitkiv.pm.2020@lpnu.ua>
2021-09-13 11:32:42 +0300

    Voitkiv-Dmytro

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-//Підключаємо gulp
-var gulp = require ("gulp");
-//Створюємо тестовий таск
-gulp.task ('testTask', function () {
- console.log ('This is a test task!');
-});
-//Запуск тасків за замовчуванням
-gulp.task ("default", ["testTask"]);
-var gulp = require ("gulp");
-//додаткові плагіни Gulp
-var sass = require ("gulp-sass"), //конвертує SASS в CSS
- cssnano = require ("gulp-cssnano"), //мінімізація CSS
- autoprefixer = require ('gulp-autoprefixer'), //додавання префіксів в
- //CSS для підтримки
- //старих браузерів
- imagemin = require ('gulp-imagemin'), //стиснення зображень
-concat = require ("gulp-concat"), //об'єднання файлів - конкатенація
- uglify = require ("gulp-uglify"), //мінімізація javascript
- rename = require ("gulp-rename"); //перейменування файлів
+function simpleTask() {
+	console.log('This is test task!');
+}
+exports.default = simpleTask
+
+const { src, dest, watch, series} = require ("gulp");
+const concat = require ("gulp-concat");
+const sass = require ("gulp-sass");
+const autoprefixer = require ("gulp-autoprefixer");
+const cssnano = require ("gulp-cssnano");
+const rename = require ("gulp-rename");
+const uglify = require ("gulp-uglify");
+const imagemin = require ("gulp-imagemin");
+
+function task_html() {
+	return src ("app/*.html")
+	.pipe(dest("dist"));
+}
+exports.html = task_html
